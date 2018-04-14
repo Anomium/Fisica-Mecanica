51a4ff93a23729bb8077fdbe40da6e8da28229ee
Anomium <melvin.martinez.97@gmail.com>
2018-04-14 16:34:35 -0500

    Combobox Unidad funcionando

---
+++ src/Controller/MedidaController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import Model.Medida;
+
+public class MedidaController {
+
+    Medida med = new Medida();
+
+    public void Calculo() {
+
+    }
+}
---
+++ src/Model/Medida.java
@@ -0,0 +1,16 @@
+package Model;
+public class Medida {
+    private double numero;
+    
+    public void Medida(double numero){this.numero = numero;}
+    
+    public double getNumero() {
+        return numero;
+    }
+
+    public void setNumero(double numero) {
+        this.numero = numero;
+    }
+    
+    
+}
